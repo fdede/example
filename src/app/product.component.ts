f4fb1179e819699a808d402da5b53678f279e487
fdede <furkandede@outlook.com.tr>
2020-12-20 16:05:36 +0300

    style binding

--- src/app/product.component.ts
+++ src/app/product.component.ts
@@ -10,7 +10,7 @@ import { ProductRepository } from './repository.model';
 export class ProductComponent {
     model: ProductRepository = new ProductRepository();
     disabled = false;
-    
+
     getClasses(id: number): string {
         let product = this.model.getProductById(id);
         return (product.price <= 1000 ? "bg-info" : "bg-secondary") + " m-2 p-2 text-white";
@@ -22,6 +22,17 @@ export class ProductComponent {
             "bg-info": product.price <= 1000,
             "bg-secondary": product.price > 1000,
             "text-center text-white": product.name == "Samsung S6"
+        }
+    }
+
+    color: string = this.model.getProductById(2).price <= 1000 ? "green" : "red";
+    fontSize: string = "25px";
+
+    getStyles(id: number) {
+        let product = this.model.getProductById(id);
+        return {
+            fontSize: "25px",
+            color: product.price <= 1000 ? "green" : "red"
         }
     }
 }
