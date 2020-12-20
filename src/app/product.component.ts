366e28597726b0adfd72c920940151132ea65c13
fdede <furkandede@outlook.com.tr>
2020-12-20 15:57:27 +0300

    Class binding

--- src/app/product.component.ts
+++ src/app/product.component.ts
@@ -9,4 +9,19 @@ import { ProductRepository } from './repository.model';
 
 export class ProductComponent {
     model: ProductRepository = new ProductRepository();
+    disabled = false;
+    
+    getClasses(id: number): string {
+        let product = this.model.getProductById(id);
+        return (product.price <= 1000 ? "bg-info" : "bg-secondary") + " m-2 p-2 text-white";
+    }
+
+    getClassMap(id: number): Object {
+        let product = this.model.getProductById(id);
+        return {
+            "bg-info": product.price <= 1000,
+            "bg-secondary": product.price > 1000,
+            "text-center text-white": product.name == "Samsung S6"
+        }
+    }
 }
